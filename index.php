91913bd362200582997e46b0a7837bde403ce132
Marouane5hoek <133367032+Marouane5hoek@users.noreply.github.com>
2023-05-12 16:11:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<br>
+<?php
+
+
+
+$int1 = 10;
+$int2 = 12;
+
+
+
+
+$int1 * $int2;
+echo $int1+$int2;
+?>
+
+
+
+<br>
+<?php
+$nummer= 10;
+$num= 12;
+echo $nummer*$num;
+?>
+<br>
+<?php
+$naam = "Marwan Khamlichi";
+$tekst = "welkom Marwan";
+echo $tekst;
+
+?>
+<?php
+
+// deze code bevat veel fouten haal alle fouten eruit!
+//
+
+
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
+
+?>
