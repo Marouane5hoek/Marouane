0e87b5d190b5f0c39d47c8bc132a98289a52c248
Marouane5hoek <133367032+Marouane5hoek@users.noreply.github.com>
2023-06-07 22:34:01 +0200

    Add files via upload
    opdracht php week 4

--- index.php
+++ index.php
@@ -1,42 +1,16 @@
-<br>
-<?php
+<?php 
 
-
-
-$int1 = 10;
-$int2 = 12;
-
-
-
+$int1 =10;
+$int2 =12;
 
 $int1 * $int2;
-echo $int1+$int2;
-?>
+echo $int1;
 
-
-
-<br>
-<?php
-$nummer= 10;
-$num= 12;
-echo $nummer*$num;
-?>
-<br>
-<?php
-$naam = "Marwan Khamlichi";
-$tekst = "welkom Marwan";
+$Naam = "Marouane Khamlichi";
+$tekst = "Welkom Marouane";
 echo $tekst;
 
-?>
-<?php
-
-// deze code bevat veel fouten haal alle fouten eruit!
-//
 
 
-
-$mijnNaam="Carl";
-$jouwNaam="Camel";
-echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
 
 ?>
